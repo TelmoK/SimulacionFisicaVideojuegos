6a48967d92c9d0d8f3f0b6c55d1e17a9250d1e08
TelmoK <telmo.kiss@gmail.com>
2025-10-18 00:14:26 +0200

    preparando Industrial Piece para transmitir fuerzas a través de los puntos de anclaje

--- skeleton/RenderItems/IndustrialPiece.h
+++ skeleton/RenderItems/IndustrialPiece.h
@@ -5,13 +5,34 @@
 class IndustrialPiece : public RenderItem
 {
 public:
+
+	struct AttachmentPoint
+	{
+		IndustrialPiece* industrial_piece;
+		AttachmentPoint* connected_point;
+	};
+
+	struct ForceTransmisionPack
+	{
+		Vector3D force;
+		Vector3D torque;
+		Vector3D torque_axis_point;
+	};
+
 	IndustrialPiece(physx::PxShape* _shape, float mass, const Vector4& _color = Vector4(1, 1, 1, 1));
 
-	virtual Vector3D reiciveLinearAcceleration();
-	virtual Vector3D reiciveAngularAcceleration();
+	/*
+		Propaga hacia delante(sus componentes conexos) un paquete de fuerzas (torque y fuerza lineal) y devuelve 
+		la fuerza reactiva resultante de la aplicación de la fuerza en los componentes conexos derivados.
+
+		AttachmentPoint se utiliza para no propagar la fuerza hacia delante al mismo AttachmentPoint que
+		está transmitiendo la fuerza, solo después de calcular la reacción se le devolverá fuerza a este
+		a través del return. 
+	*/
+	virtual ForceTransmisionPack propagateForces(AttachmentPoint* force_emitter_point);
 
 protected:
 
 	float _mass;
-	std::vector<Vector3D> _attachment_points;
+	std::vector<AttachmentPoint*> _attachment_points;
 };
--- skeleton/Utils/Vector3D.h
+++ skeleton/Utils/Vector3D.h
@@ -34,15 +34,24 @@ public:
 		z = other.z;
 	}
 
-	Vector3 to_vec3() {
+	Vector3 to_vec3() 
+	{
 		return Vector3(x, y, z);
 	}
 	
-	float magnitude() {
+	float magnitude() 
+	{
 		return sqrt(x * x + y * y + z * z);
 	}
 
 	//float dot();
 
-	//Vector3D cross();
+	Vector3D cross(const Vector3D& other)
+	{
+		return Vector3D(
+			y * other.z - z * other.y,
+			z * other.x - x * other.z,
+			x * other.y - y * other.x
+		);
+	}
 };
